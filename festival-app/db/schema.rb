3a4104c31f78371f1462523c384e91fb70eb7f12
Nicole <nicole.barnett.nz@gmail.com>
2021-03-30 20:52:21 +1300

    adding additional column to table

--- festival-app/db/schema.rb
+++ festival-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_074026) do
+ActiveRecord::Schema.define(version: 2021_03_30_074834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_074026) do
     t.string "location"
     t.integer "duration"
     t.string "description"
+    t.string "link"
   end
 
 end
