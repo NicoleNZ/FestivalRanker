1fb7909118ed98012b4dbb3e99f0f102fa15424a
Nicole <nicole.barnett.nz@gmail.com>
2021-03-30 22:22:29 +1300

    added user migration

--- festival-app/db/schema.rb
+++ festival-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_074834) do
+ActiveRecord::Schema.define(version: 2021_03_30_091925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2021_03_30_074834) do
     t.integer "duration"
     t.string "description"
     t.string "link"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
   end
 
 end
